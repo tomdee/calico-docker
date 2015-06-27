dc3a5551d15588e5da616cc14dd994d9d8a9361a
Tom Denham <tom@tomdee.co.uk>
2015-06-26 22:27:42 -0700

    Images builds and runs, but there are issues...
    - Felix crashes because of an IPIP issue
    - BIRD isn't installed yet.

--- image/install.sh
+++ image/install.sh
@@ -1,24 +1,14 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-# Get the current list of required packages (this is used in the clean up
-# code to remove the temporary packages that are about to be installed).
-dpkg -l | grep ^ii | sed 's_  _\t_g' | cut -f 2 >/tmp/required.txt
-
-# Install temporary packages required for installing Felix and etcd.
-apt-get install -qy \
-        git \
-        python-dev \
-        libffi-dev \
-        libssl-dev
-
-# Install Felix and python-etcd from the Metaswitch github repos.
-pip install git+https://github.com/Metaswitch/python-etcd.git
-pip install git+https://github.com/Metaswitch/calico.git@0.27
+#!/usr/bin/env sh
+apk update
+apk add python
+apk add --virtual build-dependencies python-dev build-base curl bash py-pip alpine-sdk libffi-dev openssl-dev
+pip install -r /pycalico/requirements.txt
 
 # Install Confd
 curl -L https://www.github.com/kelseyhightower/confd/releases/download/v0.9.0/confd-0.9.0-linux-amd64 -o confd
 chmod +x confd
 
-pip install -r /pycalico/requirements.txt
+pip install git+https://github.com/Metaswitch/python-etcd.git
+pip install git+https://github.com/Metaswitch/calico.git@0.27
+
+# BIRD
